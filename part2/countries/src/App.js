52450f5c4b4200001eb17f9a87fc27e079f60a14
andy <andy.william.mcdonald@gmail.com>
2020-09-24 14:43:08 +0100

    part 2.13 done

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,47 +1,62 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-const Display = ({ countriesToShow }) => {
-  
-  return(
-    <div>
-    { countriesToShow.length > 10 ? (
-      <p>Too many matches, specify another filter</p>
-    ) : countriesToShow.length > 1 ? (
-      <ul>
+const TooManyMatches = () => {
+  return <p>Too many matches, specify another filter</p>;
+};
+
+const ListView = ({ countriesToShow, onClick }) => {
+  return (
+    <ul>
       {countriesToShow.map((x) => (
-        <li key={x.name}>{x.name}</li>
+        <li key={x.name}>
+          {x.name}
+          <button id={x.name} onClick={onClick}>show</button>
+        </li>
       ))}
     </ul>
-    ) : (
-      <>
+  );
+};
+
+const CountryDetail = ({ countriesToShow }) => {
+  return (
+    <>
       {countriesToShow.map((x) => (
         <div key={x.name}>
-        <h1>{x.name}</h1>
-        <p>capital {x.capital}</p>
-        <p>population {x.population}</p>
-        <h3>Languages</h3>
-        <ul>
-          {x.languages.map((y)=> (
-            <li key={y.name}>{y.name}</li>
-          ))}
-        </ul>
-        <img src={x.flag} width="300px" />
+          <h1>{x.name}</h1>
+          <p>capital {x.capital}</p>
+          <p>population {x.population}</p>
+          <h3>Languages</h3>
+          <ul>
+            {x.languages.map((y) => (
+              <li key={y.name}>{y.name}</li>
+            ))}
+          </ul>
+          <img src={x.flag} width="300px" alt="country flag" />
         </div>
       ))}
-     </>
-    )
-  }
-  </div>
-  )
-}
+    </>
+  );
+};
 
+const Display = ({ countriesToShow, onClick }) => {
+  return (
+    <div>
+      {countriesToShow.length > 10 ? (
+        <TooManyMatches />
+      ) : countriesToShow.length > 1 ? (
+        <ListView countriesToShow={countriesToShow} onClick={onClick} />
+      ) : (
+        <CountryDetail countriesToShow={countriesToShow} />
+      )}
+    </div>
+  );
+};
 
 const App = () => {
   const [countriesAll, setCountriesAll] = useState([]);
   const [searchInput, setSearchInput] = useState("");
   const [filter, setFilter] = useState(false);
-  
 
   useEffect(() => {
     console.log("effect");
@@ -62,7 +77,11 @@ const App = () => {
         x.name.toUpperCase().includes(searchInput.toUpperCase())
       );
 
-console.log(countriesToShow);
+  // console.log(countriesToShow);
+
+  const handleClick = (e) => {
+    setSearchInput(e.target.id)
+  }
 
   return (
     <>
@@ -71,12 +90,7 @@ console.log(countriesToShow);
         <input value={searchInput} onChange={handleSearch} />
       </div>
       <div>
-        {/* <ul>
-          {countriesToShow.map((x) => (
-            <li key={x.name}>{x.name}</li>
-          ))}
-        </ul> */}
-        <Display countriesToShow={countriesToShow} />
+        <Display countriesToShow={countriesToShow} onClick={handleClick} />
       </div>
     </>
   );
